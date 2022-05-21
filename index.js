e94c705ad6125eb9fdcff098dbcc23f0fc7c272f
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-05-21 11:59:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express"),
     route = require("./routes/route"),
     connectToDb = require("./db/db"),
     MONGO_URL = process.env.MONGO_URL;
-const hostName = '0.0.0.0';
+const hostName = 'localhost';
 const cors = require("cors");
 
 
