3f6c715c4574bb2285c002940a5ce9b77bdc97f6
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-05-21 10:17:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express"),
     route = require("./routes/route"),
     connectToDb = require("./db/db"),
     MONGO_URL = process.env.MONGO_URL;
+const hostName = '0.0.0.0';
 const cors = require("cors");
 
 
@@ -18,7 +19,7 @@ app.use('/api/v1', route);
 
 
 connectToDb(MONGO_URL, () => {
-    app.listen(PORT, () => {
-        console.log(`server is running on http://localhost:${PORT}`);
+    app.listen(PORT,hostName, () => {
+        console.log(`server is running on http://${hostName}:${PORT}`);
     })
 })
